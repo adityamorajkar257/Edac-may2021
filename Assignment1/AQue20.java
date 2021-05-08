97e171887858ef51228c3405220ffee6021789cd
aditya morajkar <83824383+adityamorajkar257@users.noreply.github.com>
2021-05-08 21:06:19 +0530

    Add files via upload

---
+++ Assignment1/AQue20.java
@@ -0,0 +1,23 @@
+public class AQue20
+{
+
+
+   public static void main(String args[])
+   {
+     int binary[]=new int[20];
+     int index=0;
+     int n=5;
+     while(n>0)
+     {
+        binary[index++]=n%2;
+         n=n/2;
+     }
+    
+     for(int i=index-1;i>=0;i--)
+     {
+          System.out.print(binary[i]);
+     }
+  
+    }
+
+ }
---
+++ Assignment1/AQue24.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class AQue24
+{
+ public static void main(String args[])
+ {
+  int num1;
+Scanner sc=new Scanner(System.in);
+num1=Integer.parseInt(sc.nextLine(),2);
+System.out.println(num1);
+  }
+}
